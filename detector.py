78c30d5e2209f12f15d2f5f8729da4704adff19a
entropyQueen <emy.rg@protonmail.com>
2021-12-12 22:45:34 +0100

    Added phorcys to audo decode payload in order to detect more log4shell exploits

--- detector.py
+++ detector.py
@@ -3,10 +3,13 @@ import asyncore
 import socket
 import logging
 import time
-import re
-
+import json
 import sys
+import re
+import os
 from threading import Thread
+from phorcys.decoders.deepdecoder import DeepDecoder
+from phorcys.inspectors.yara_inspector import YaraInspector
 
 HOST = ''
 PORT = 8080
@@ -14,11 +17,15 @@ PORT = 8080
 # Create console\stream handler
 logging.basicConfig(
     format='%(asctime)s %(levelname)s:%(message)s',
-    level=logging.DEBUG
+    level=logging.INFO
 )
 # Create file handler
-fh = logging.FileHandler('catcher.log')
-fh.setLevel(logging.DEBUG)
+try:
+    os.mkdir('logs')
+except FileExistsError:
+    pass
+fh = logging.FileHandler('logs/catcher.log')
+fh.setLevel(logging.INFO)
 # Create formatter
 formatter = logging.Formatter('%(asctime)s %(levelname)s:%(message)s')
 fh.setFormatter(formatter)
@@ -31,31 +38,42 @@ class Handler(asyncore.dispatcher_with_send):
     def __init__(self, sock, addr):
         super().__init__(sock)
         self.addr = addr
+        self.dd = DeepDecoder()
 
     def handle_read(self):
-        data = self.recv(16384)
+        data = self.recv(16384).strip()
         if data:
             if not data:
                 return
-            x = re.findall(b'\$\{.*//(.*)\}', data)
-            if len(x) >= 1:
-                for p in x:
-                    p = p.decode('utf-8')
-                    logging.info(f'Found payload jndi from {repr(self.addr)}')
-                    logging.info(f'URL: {p}')
-                    with open(f'logs_{int(time.time())}_data.bin', 'wb') as f:
-                        f.write(data)
-                    try:
-                        h, p = p.split('/')[0].split(':')
-                    except:
-                        h = p.split('/')[0]
-                        p = 389
 
-                    Thread(target=self.get_payload, args=[h, int(p)]).start()
+            layer = self.dd.decode(data)
+            inspector = YaraInspector(open('log4j_exploit.yara', 'r').read())
+            inspector(layer)
+            for leaf in layer.leaves:
+                if leaf._matching_rules:
+                    for rule in leaf._matching_rules:
+                        rule_name = rule.get('rule')
+                        if 'og4' in rule_name:
+                            with open(f'logs/logs_{int(time.time())}_data.bin', 'wb') as f:
+                                f.write(data)
+                            self.analyze_payload(data, leaf.raw_data)
+
+    def analyze_payload(self, data, payload):
+        x = re.findall('\$\{.*?\/\/(.*?)\}', payload)
+        if len(x) >= 1:
+            for p in x:
+                logging.info(f'Found payload jndi from {repr(self.addr)}')
+                logging.info(f'URL: {p}')
+                try:
+                    h, p = p.split('/')[0].split(':')
+                except:
+                    h = p.split('/')[0]
+                    p = 389
+                Thread(target=self.get_payload, args=[h, int(p)]).start()
 
     def get_payload(self, h, p):
         try:
-            with open(f'logs_{int(time.time())}.bin', 'wb') as f:
+            with open(f'logs/logs_{int(time.time())}.bin', 'wb') as f:
                 s = socket.socket()
                 s.connect((h, p))
                 r = s.recv(16384)
